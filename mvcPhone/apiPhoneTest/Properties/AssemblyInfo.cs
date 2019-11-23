a4fb55d55491b67128b434fe80684ae1dab922f8
Fer Torrejon <fernandatorrejon24@gmail.com>
2019-11-23 13:55:07 -0400

    Tests

---
+++ mvcPhone/apiPhoneTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("apiPhoneTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("apiPhoneTest")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("d0df1bbf-c405-433f-a469-eadbdbe743d7")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ mvcPhone/apiPhoneTest/UnitTest1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace apiPhoneTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestControllerExistePhoneExists()
+        {
+            //Organizar
+
+            var controller = new apiPhone.Controllers.PhonesController();
+
+            //Actuar
+
+            var reponse = controller.PhoneExists(1);
+
+            //Afirmar
+
+            Assert.AreEqual(false, reponse);
+        }
+    }
+}
